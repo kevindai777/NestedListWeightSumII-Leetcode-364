5ea96975bcd5992f075ec2cec8482318d9b55edb
kevindai777 <kevin.dai.2451@gmail.com>
2020-07-01 03:13:54 -0400

    solutions

---
+++ nestedlistweightsumii.js
@@ -0,0 +1,33 @@
+//Objective is similar to 'Nested List Weight Sum II', except with inverted depths.
+
+let list = [[1,1],2,[1,1]]
+
+
+//O(n) solution that finds the deepest depth first and then uses it to get the sum.
+
+function getSum(list, depth) {
+    let sum = 0
+
+    for (let element of list) {
+        if (isFinite(element)) {
+            sum += element * depth
+        } else {
+            sum += getSum(element, depth - 1)
+        }
+    }
+
+    return sum
+}
+
+function getMaxDepth(list) {
+    let depth = 0
+    for (let element of list) {
+        if (!isFinite(element)) {
+            depth = Math.max(depth, getMaxDepth(element))
+        }
+    }
+
+    return depth + 1
+}
+
+return getSum(list, getMaxDepth(list))
